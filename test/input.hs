8ea6707761d8ba62b32b880d6dec3d5e7ed44287
Brandon Chinn <brandonchinn178@gmail.com>
2023-02-03 09:33:27 -0800

    Add more to input.hs

--- test/input.hs
+++ test/input.hs
@@ -8,15 +8,20 @@ module MyModule (
   (*+*),
 ) where
 
+import Control.Monad (when)
+
 -- | This is a comment
 class MyClass a where
   foo :: a -> a
 
 data MyType a = MyCon {conField1 :: Int, conField2 :: a}
 
-func1 :: forall a. Show a => a -> [a] -> (a, [a])
-func1 a [] = (a, [])
-func1 a (_ : end) = (a, end)
+func1 :: forall a. (MonadIO m, Show a) => a -> [a] -> m (a, [a])
+func1 a [] = pure (a, [])
+func1 a (_ : end) = do
+  debug <- getEnv "DEBUG"
+  when debug $ print end
+  pure (a, end)
 
 (*+*) :: Int -> Int -> Bool
-a *+* b = (a * 2) == b
+a *+* b = (a * 2) == b && odd a
