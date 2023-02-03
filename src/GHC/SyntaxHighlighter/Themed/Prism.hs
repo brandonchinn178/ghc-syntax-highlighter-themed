8cf22afcbb91352c98347b576dd5b4b958cad121
Brandon Chinn <brandonchinn178@gmail.com>
2023-02-03 09:35:27 -0800

    Clean up provider-specific modules

--- src/GHC/SyntaxHighlighter/Themed/Prism.hs
+++ src/GHC/SyntaxHighlighter/Themed/Prism.hs
@@ -3,10 +3,6 @@
 
 module GHC.SyntaxHighlighter.Themed.Prism (
   renderHaskell,
-
-  -- * Manual processing
-  renderTokens,
-  renderToken,
   config,
 ) where
 
@@ -16,18 +12,11 @@ import GHC.SyntaxHighlighter.Themed
 
 -- | The primary entrypoint that will tokenize Haskell code with
 -- @ghc-syntax-highlighter@ and render HTML compatible with
--- Prism themes.
+-- [Prism](https://prismjs.com) themes.
 --
 -- Returns Nothing if the input could not be parsed.
 renderHaskell :: Text -> Maybe Text
 renderHaskell = renderHaskellWith config
-
--- | Convert tokens output by 'tokenizeHaskell' into an HTML snippet.
-renderTokens :: [(Token, Text)] -> Text
-renderTokens = renderTokensWith config
-
-renderToken :: (Token, Text) -> Text
-renderToken = renderTokenWith config
 
 config :: ThemeProviderConfig
 config =
